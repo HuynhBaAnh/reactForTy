e2326b24cda06cef0be5b96ebb3f5001d8777362
Huynh Ba Anh <huynhbaanh03112003@@gmail.com>
2024-10-18 23:25:54 +0700

    create project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='flex justify-center container mx-auto bg-red-300 border border-red'>
+        <p>haha</p>
+      </div>
+    </>
+  )
+}
+
+export default App
