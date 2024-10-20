89f77d10900c90028849cfbdb076f1a5983f13a3
tester <hoangthive124@gmail.com>
2024-10-20 15:03:42 +0700

    hnay20/10/24 component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Button from './component/Button'
 
 function App() {
   return (
@@ -6,6 +7,12 @@ function App() {
       <div className='flex justify-center container mx-auto bg-red-300 border border-red'>
         <p>haha</p>
       </div>
+
+      <Button dieukien={false} content='hihi'/>
+      <br/>
+      <Button dieukien icon = 'mặt cười' content='hihi'/>
+
+    
     </>
   )
 }
