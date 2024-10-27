f8b57012d6a3eb1d5a4d60a8bcc7cbae34c1c243
tester <hoangthive124@gmail.com>
2024-10-27 22:23:53 +0700

    hni học ...

--- src/component/Item.tsx
+++ src/component/Item.tsx
@@ -8,7 +8,7 @@ type Props = {
 export default function Item({image, text}: Props) {
   return (
     <>
-    <div className='flex border rounded-lg w-96 items-center space-x-5 p-2 mt-6 ml-3'>
+    <div className='flex border rounded-lg w-96 items-center space-x-5 m-3  '>
     <img className='w-20 h-20 rounded-xl 
     ' src={image} alt="" />
     <div>
